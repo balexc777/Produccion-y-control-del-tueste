2a0ab074b5f1b898caae7f5615a8edfe272569cb
Martinnba <mbaigorria388@gmail.com>
2026-06-02 21:15:40 -0300

    Task-003: Add: se implementó la intensidad de la curva del tueste

--- validador.py
+++ validador.py
@@ -1,6 +1,26 @@
 
 while True:
     try:
+        intensidad = int(input("Seleccione la intensidad de la curva del tueste:\n 1-suave\n 2-medio\n 3-intensad\n opcion:"))
+        if intensidad in [1, 2, 3]:
+            break
+        else:
+            print("Error. Debe ingresar 1, 2 o 3.")
+
+    except ValueError:
+        print("Error: Debe ingresar un número.")
+if intensidad == 1:
+    temp_min = 180
+    temp_max = 200
+elif intensidad == 2:
+    temp_min = 200
+    temp_max = 220
+else:
+    temp_min = 220
+    temp_max = 240                
+
+while True:
+    try:
         minutos = int(input("Ingrese cuántos minutos va a durar el lote: "))
 
         if minutos <= 0:
@@ -22,3 +42,4 @@ for i in range(minutos):
             print("Error: Debe ingresar un número.")
 
 print("Las temperaturas se registraron con éxito.")
+

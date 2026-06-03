28a7f1a5b98315d8ea3442268969611e8b3adcb4
Martinnba <mbaigorria388@gmail.com>
2026-06-02 23:07:42 -0300

    Task-004: Update: Se modificó codigo de task-003

--- validador.py
+++ validador.py
@@ -1,24 +1,23 @@
 
 while True:
     try:
-        intensidad = int(input("Seleccione la intensidad de la curva del tueste:\n 1-suave\n 2-medio\n 3-intensad\n opcion:"))
+        intensidad = int(input("Seleccione la intensidad de la curva del tueste:\n 1-suave\n 2-medio\n 3-intensa\n opcion:"))
         if intensidad in [1, 2, 3]:
+            if intensidad == 1:
+                temp_min = 180
+                temp_max = 200
+            elif intensidad == 2:
+                temp_min = 200
+                temp_max = 220
+            else:
+                temp_min = 220
+                temp_max = 240
             break
         else:
             print("Error. Debe ingresar 1, 2 o 3.")
 
     except ValueError:
-        print("Error: Debe ingresar un número.")
-if intensidad == 1:
-    temp_min = 180
-    temp_max = 200
-elif intensidad == 2:
-    temp_min = 200
-    temp_max = 220
-else:
-    temp_min = 220
-    temp_max = 240                
-
+        print("Error: Debe ingresar un número.")              
 while True:
     try:
         minutos = int(input("Ingrese cuántos minutos va a durar el lote: "))
